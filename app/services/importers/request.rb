81e42381dfc54b139d8fa3ae88ab4e2d215e1a20
Matthew Morris <matthew.morris@cessda.eu>
2026-07-27 16:16:44 +0200

    Only set Authorization if a token is passed

--- app/services/importers/request.rb
+++ app/services/importers/request.rb
@@ -6,7 +6,7 @@ class Importers::Request
   def initialize(eosc_registry_base_url, suffix, faraday: Faraday, token: nil, id: nil)
     @eosc_registry_base_url = eosc_registry_base_url
     @suffix = suffix
-    @token = "Bearer #{token}"
+    @token = "Bearer #{token}" unless token == nil
     @id = id
     @conn = api_client_connection(faraday, @token)
   end
